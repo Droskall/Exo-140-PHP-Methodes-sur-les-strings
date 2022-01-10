3109d487a72c81dcae6a7bdbbbd59f0ba69e2775
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-10 15:10:06 +0100

    finititifinitoto

--- index.php
+++ index.php
@@ -2,22 +2,22 @@
 
 // Compléter le code ci dessous pour afficher la longueur de la chaine de caracteres
 
-echo      ("google");
+echo strlen("google")."<br>";
 
 //echo"<br><br>"; //Decommentez les <br> pour tester l'affichage lorsque vous aurez plusieurs instructions
 
 // Compléter le code ci dessous pour inverser la chaine de caractere
 
-echo  ("anticonstitutionellement");
+echo strrev("anticonstitutionellement")."<br>";
 
 // Et si vous testiez également avec votre prénom ?
 
-
+echo strrev("dada")."<br>";
 
 //echo"<br><br>";
 
 // Compléter le code ci dessous pour remplacer le mot ok par non
 $oldtxt = "Ok Google!";
-$newtxt = (, , );
+$newtxt = str_replace("Ok", "Non" , $oldtxt );
 
 echo $newtxt;
